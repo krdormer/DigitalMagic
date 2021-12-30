4964af49fe4a59abd8259192c5772f83d7da4835
Kevin  Dormer <kevindormer@Kevins-MacBook-Pro.local>
2021-12-29 20:29:49 -0600

    Add node-sass, update header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const headerStyles = {
-  display: 'flex',
-  alignItems: 'center',
-  height: '8vh',
-};
-
-const linkStyles = {
-  marginLeft: '2%',
-  textDecoration: 'none',
-  color: '#0077B6',
-  cursor: 'pointer',
-  fontSize: '1.5em',
-  textShadow: '2px 2px rgb(200, 200, 200)',
-};
+import styles from './styles.module.scss'
 
 const Header = () => (
-  <div className="header" style={headerStyles}>
-    <Link to="/" style={linkStyles}>
-      Home
-    </Link>
+  <div className={styles.header}>
+    <Link to="/" className={styles.headerLink}>Home</Link>
   </div>
 );
 
